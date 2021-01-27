29c72bf030e7bcee833fd044bc394a1212a83793
Bruno Bandeira <brunodev29ab@gmail.com>
2021-01-27 16:34:55 -0300

    Aula 20, Ajuste no endpoint/Categorias/{id}

--- src/main/java/com/brunobandeira/cursomc/services/CategoriaService.java
+++ src/main/java/com/brunobandeira/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.brunobandeira.cursomc.domain.Categoria;
 import com.brunobandeira.cursomc.repositories.CategoriaRepository;
+import com.brunobandeira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,10 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ " , Tipo: " + Categoria.class.getName());
+		}
 		return obj.orElse(null);
 	}
 }
